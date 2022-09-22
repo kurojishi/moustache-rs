ae307dc20e9f29a2389b7df8deaff98bfc79080e
Francesco Berni <kurojishi@kurojishi.me>
2022-09-22 12:24:36 +0200

    Opencv4 doesn't use specifc haar cascade for nose detection

--- src/main.rs
+++ src/main.rs
@@ -14,15 +14,14 @@ struct Cli {
 
 const CASCADE_BASE: &str = "/usr/share/opencv4/haarcascades";
 const CASCADE_FACE: &str = "haarcascade_frontalface_default.xml";
-const CASCADE_NOSE: &str = "/home/ANT.AMAZON.COM/fraber/src/moustache-rs/static/haarcascade_mcs_nose.xml";
-const MOUSTACHE_PATH: &str = "/home/ANT.AMAZON.COM/fraber/src/moustache-rs/static/mustache.png";
+const MOUSTACHE_PATH: &str = "static/mustache.png";
 
 fn main() {
     let args = Cli::parse();
     let source = args.source;
     let destination = args.destination;
     let cascade_face: String = format!("{CASCADE_BASE}/{CASCADE_FACE}");
-    let moustacher = match Moustacher::new(cascade_face, String::from(CASCADE_NOSE), String::from(MOUSTACHE_PATH)) {
+    let moustacher = match Moustacher::new(cascade_face, String::from(MOUSTACHE_PATH)) {
         Ok(moustacher) => moustacher,
         Err(err) => panic!("Failed to initialize moustacher: {:?}", err),
     };
--- src/moustacher.rs
+++ src/moustacher.rs
@@ -3,34 +3,29 @@ use opencv::imgcodecs::{imread, imwrite, IMREAD_UNCHANGED, IMWRITE_JPEG_QUALITY}
 use opencv::imgproc::{cvt_color, resize, COLOR_BGR2GRAY, INTER_AREA};
 use opencv::objdetect::{CascadeClassifier, CASCADE_SCALE_IMAGE};
 use opencv::prelude::*;
+use opencv::types::VectorOfRect;
 use opencv::Error;
+
 
 pub struct Moustacher {
     cascade_face: CascadeClassifier,
-    cascade_nose: CascadeClassifier,
     moustache_original: Mat,
 }
 
 impl Moustacher {
-    pub fn new(cascade_face_path: String, cascade_nose_path: String, moustache_path: String) -> Result<Self, Error> {
+    pub fn new(cascade_face_path: String, moustache_path: String) -> Result<Self, Error> {
         let stache_img = imread(&moustache_path, IMREAD_UNCHANGED)?;
         println!("faces path: {cascade_face_path}");
         Ok(Self {
             cascade_face: CascadeClassifier::new(&cascade_face_path)?,
-            cascade_nose: CascadeClassifier::new(&cascade_nose_path)?,
             moustache_original: stache_img,
         })
     }
 
-    pub fn add_moustache_to_image(mut self, source: String, destination: String) -> Result<(), Error> {
-        println!("{:?}", self.moustache_original);
-        let image = imread(&source, IMREAD_UNCHANGED)?;
-        println!("{:?}", image);
-        let mut image_gray_scale = Mat::default();
-        cvt_color(&image, &mut image_gray_scale, COLOR_BGR2GRAY, 0)?;
+    fn detect_faces(mut self, image: Mat) -> Result<VectorOfRect, Error> {
         let mut faces: Vector<Rect> = Vector::new();
         self.cascade_face.detect_multi_scale(
-            &image_gray_scale,
+            &image,
             &mut faces,
             1.1,
             5,
@@ -38,49 +33,25 @@ impl Moustacher {
             Size::new(30, 30),
             Size::default(),
         )?;
-        println!("{:?}", faces);
+        println!("Faces {:?}", faces);
+        Ok(faces)
+
+    }
+
+
+    pub fn add_moustache_to_image(self, source: String, destination: String) -> Result<(), Error> {
+        println!("moustache parsed {:?}", self.moustache_original);
+        let image = imread(&source, IMREAD_UNCHANGED)?;
+        println!("Image parsed {:?}", image);
+        let mut image_gray_scale = Mat::default();
+        cvt_color(&image, &mut image_gray_scale, COLOR_BGR2GRAY, 0)?;
+        let faces = self.detect_faces(image_gray_scale.clone())?;
+        println!("Faces {:?}", faces);
         for face_location in faces {
-            let roi_gray = Mat::roi(&image_gray_scale, face_location)?;
-            let roi_color = Mat::roi(&image, face_location)?;
-            let mut noses: Vector<Rect> = Vector::new();
-            self.cascade_nose
-                .detect_multi_scale(&roi_gray, &mut noses, 1.1, 3, 0, Size::default(), Size::default())?;
-            if noses.is_empty() {
-                panic!("NO NOSE");
+            let _roi_gray = Mat::roi(&image_gray_scale, face_location)?;
+            let _roi_color = Mat::roi(&image, face_location)?;
+            let mut _noses: Vector<Rect> = Vector::new();
             }
-            println!("{:?}", noses);
-            for nose in noses {
-                let stache_width = 3 * nose.width;
-                let stache_height = stache_width * self.moustache_original.rows() / self.moustache_original.cols();
-                let mut x1 = nose.x - (stache_width / 4);
-                let mut x2 = nose.x + nose.width + (stache_width / 4);
-                let mut y1 = nose.y + nose.height - (stache_height / 4);
-                let mut y2 = nose.y + nose.height + (stache_height / 4);
-                if x1 < 0 {
-                    x1 = 0;
-                };
-                if y1 < 0 {
-                    y1 = 0;
-                }
-                if x2 > face_location.width {
-                    x2 = face_location.width;
-                }
-                if y2 > face_location.height {
-                    y2 = face_location.height;
-                }
-                let stache_width = x2 - x1;
-                let stache_height = y2 - y1;
-                let mut final_mustache = Mat::default();
-                resize(
-                    &self.moustache_original,
-                    &mut final_mustache,
-                    Size::new(stache_width, stache_height),
-                    0.0,
-                    0.0,
-                    INTER_AREA,
-                )?;
-            }
-        }
         let params: Vector<i32> = Vector::from_slice(&[IMWRITE_JPEG_QUALITY, 90]);
         imwrite(&destination, &image_gray_scale, &params)?;
 
@@ -96,7 +67,6 @@ mod test {
     fn test_moustache_initalization() {
         Moustacher::new(
             String::from("/usr/share/opencv4/haarcascades/haarcascade_frontalface_default.xml"),
-            String::from("/usr/share/opencv4/haarcascades/haarcascade_mcs_nose.xml"),
             String::from("../static/mustache.png"),
         )
         .unwrap();
